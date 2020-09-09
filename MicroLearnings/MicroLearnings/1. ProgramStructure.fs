91d816ea0cabbe3b0ef04c713c827d78f26c12a3
Mohit Kalra <mk@Mohits-MacBook-Pro.local>
2020-09-09 11:49:55 -0400

    Sep 9 2020

---
+++ MicroLearnings/MicroLearnings/1. ProgramStructure.fs
@@ -0,0 +1,24 @@
+﻿(*
+- functions work like data types
+- declare and use a function like any other variable
+- to make the program work like a procedural program, many applications keep a single top level statement
+*)
+
+open System
+(* multi line comment *)
+// single line comment
+
+
+// function called sign, takes in num as parameter
+let sign num = 
+	if num > 0 then "positive"
+	elif num < 0 then "negative"
+	else "zero"
+
+// main function calls sign
+let main() =
+	Console.WriteLine("sign 5 : {0}", (sign 5))
+
+// call main() function
+main()
+
---
+++ MicroLearnings/MicroLearnings/4. Variables.fs
@@ -0,0 +1,31 @@
+﻿(*
+
+- let keyword is used for variable declaration
+- variables in F# are immutable, which means, once assigned it can't be changed
+- mutable keyword allows you to declare and assign values in a mutable variable
+- use <- operator to re-assign value to a mutable variable
+
+*)
+
+// simple declaration
+let x = 10
+let y = 20
+let z = x + y
+printfn "x: %i" x
+printfn "y: %i" y
+printfn "z: %i" z
+
+
+// declaration with type specification
+let a:int32 = 10
+let b:int32 = 20
+let c:int32 = a + b
+printfn "a : %i" a
+printfn "b : %i" b
+printfn "c : %i" c
+
+
+// mutable variables
+let mutable d = 10
+d <- 20
+printfn "d : %i" d
