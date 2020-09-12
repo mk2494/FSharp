681feed3613459cd33f7201c6beebe82151c51db
Mohit Kalra <mk@Mohits-MacBook-Pro.local>
2020-09-11 20:53:50 -0400

    Operators in F#

---
+++ MicroLearnings/MicroLearnings/5. Operators.fs
@@ -0,0 +1,28 @@
+(*
+
++   add
+-   subtract
+*   multiply
+/   Divide
+%   Remainder
+**  Exponentiation
+
+
+=  equal
+<>  equal
+>   greater than
+<   less than
+>= greater than equal to
+<= less than equal to
+
+&&  Boolean AND
+||  Boolean OR
+not Boolean NOT
+
+&&& Bitwise AND
+||| Bitwise OR
+^^^ Bitwise EX-OR
+~~~ Bit Flipping
+<<< Binary Left  Shift
+>>> Binary Right Shift
+*)
